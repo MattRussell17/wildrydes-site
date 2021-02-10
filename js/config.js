e1a91d6034320ce3914398974b7774db3284c641
MattRussell17 <77754367+MattRussell17@users.noreply.github.com>
2021-02-10 17:57:29 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_ri6FYHNmj', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1d4e9h7k5au1arlc05jhie3r37', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_JNr5VVnQB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'mgei74l99pkr9pe7qfc37v1s3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
