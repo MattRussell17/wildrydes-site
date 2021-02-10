7e906c1627beb60c9f3d28f36be6e1a2645c14d0
MattRussell17 <77754367+MattRussell17@users.noreply.github.com>
2021-02-10 17:51:59 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_ri6FYHNmj', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-2_ri6FYHNmj', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1d4e9h7k5au1arlc05jhie3r37', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
@@ -8,3 +8,4 @@ window._config = {
         invokeUrl: 'https://0j2u0heoy4.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 }; 
+
